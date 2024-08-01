abd2a68cfc9e8578035afd1c566d27f9b876f29d
Umoh Samuel Godwin <umohsg@gmail.com>
2024-08-01 10:43:04 +0100

    login and pin input

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "FlowOptix | Home",
+  description: "Home for FlowOptix",
+};
+
+export default function Home() {
+  return <main>hello world</main>;
+}
