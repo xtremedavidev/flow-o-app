079da6c84d2413b02627a8c305d40fa695958ae8
Umoh Samuel Godwin <umohsg@gmail.com>
2024-08-01 18:49:10 +0100

    current update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import PinInput from "react-pin-input";
 
@@ -19,11 +19,17 @@ const Login = () => {
     formState: { errors },
   } = useForm<LoginInputs>();
 
+  const [currentTab, setCurrentTab] = useState(0);
+
   const onSubmit: SubmitHandler<LoginInputs> = async (data) => {
     const UserData = {
       email: data.email,
       password: data.password,
     };
+
+    console.log("hee", UserData);
+
+    setCurrentTab(1);
   };
 
   return (
@@ -36,78 +42,101 @@ const Login = () => {
         className="absolute left-0 top-1/2 z-0 h-[75%] w-auto -translate-y-1/2"
       />
 
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="relative z-[1] flex w-full max-w-[890px] flex-col gap-10 rounded-[36px] border-[1.5px] border-solid border-[#297FB8] bg-[#272727] p-12 2xl:p-20"
-      >
-        <div className="space-y-2 text-center">
-          <h1 className="text-2xl font-semibold">Welcome Back,</h1>
-          <p className="text-base font-normal">Please Log in to continue</p>
-        </div>
-
-        <div className="flex flex-col gap-[44px]">
-          {loginArr.map(({ name, label, type }) => (
-            <LoginInput
-              key={name}
-              type={type as "email" | "password"}
-              placeholder={`Enter your ${label}`}
-              label={label}
-              error={errors[name]?.message as string}
-              {...register(name, {
-                required: `${label} is required`,
-              })}
-            />
-          ))}
-        </div>
-
-        {/* <div className="flex flex-col items-center gap-[44px]">
-        
-
-          <PinInput
-            length={5}
-            initialValue=""
-            secret
-            secretDelay={100}
-            onChange={(value, index) => {}}
-            type="numeric"
-            inputMode="number"
-            style={{ padding: "10px", gap: "20px" }}
-            inputStyle={{
-              borderRadius: "13px",
-              backgroundColor: "#464646",
-            }}
-            inputFocusStyle={{
-              borderColor: "blue",
-              borderRadius: "13px",
-              backgroundColor: "#464646",
-            }}
-            onComplete={(value, index) => {}}
-            autoSelect={true}
-            regexCriteria={/^[ A-Za-z0-9_@./#&+-]*$/}
-          />
-        </div> */}
-
-        <div className="flex items-center justify-between text-sm font-normal">
-          <div className="flex gap-2">
-            <input type="checkbox" />
-            <p>Remember me</p>
+      {currentTab === 0 ? (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="relative z-[1] flex w-full max-w-[890px] flex-col gap-10 rounded-[36px] border-[1.5px] border-solid border-[#297FB8] bg-[#272727] p-12 2xl:p-20"
+        >
+          <div className="space-y-2 text-center">
+            <h1 className="text-2xl font-semibold">Welcome Back,</h1>
+            <p className="text-base font-normal">Please Log in to continue</p>
           </div>
 
-          <p>
-            Forgot your Password?{" "}
-            <Link href="" className="font-bold">
-              Reset
-            </Link>
-          </p>
-        </div>
+          <div className="flex flex-col gap-[44px]">
+            {loginArr.map(({ name, label, type }) => (
+              <LoginInput
+                key={name}
+                type={type as "email" | "password"}
+                placeholder={`Enter your ${label}`}
+                label={label}
+                error={errors[name]?.message as string}
+                {...register(name)}
+              />
+            ))}
+          </div>
 
-        <button
-          type="submit"
-          className="h-[48px] w-full items-center justify-center rounded-[17px] bg-[#297FB8] text-base font-semibold"
+          <div className="flex items-center justify-between text-sm font-normal">
+            <div className="flex gap-2">
+              <input type="checkbox" />
+              <p>Remember me</p>
+            </div>
+
+            <p>
+              Forgot your Password?{" "}
+              <Link href="" className="font-bold">
+                Reset
+              </Link>
+            </p>
+          </div>
+
+          <button
+            type="submit"
+            className="h-[48px] w-full items-center justify-center rounded-[17px] bg-[#297FB8] text-base font-semibold"
+          >
+            Log In
+          </button>
+        </form>
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="relative z-[1] flex w-full max-w-[890px] flex-col gap-10 rounded-[36px] border-[1.5px] border-solid border-[#297FB8] bg-[#272727] p-12 2xl:p-20"
         >
-          Log In
-        </button>
-      </form>
+          <div className="flex flex-col items-center gap-[44px]">
+            <PinInput
+              length={5}
+              initialValue=""
+              secret
+              secretDelay={100}
+              onChange={(value, index) => {}}
+              type="numeric"
+              inputMode="number"
+              style={{ padding: "10px", gap: "20px" }}
+              inputStyle={{
+                borderRadius: "13px",
+                backgroundColor: "#464646",
+              }}
+              inputFocusStyle={{
+                borderColor: "blue",
+                borderRadius: "13px",
+                backgroundColor: "#464646",
+              }}
+              onComplete={(value, index) => {}}
+              autoSelect={true}
+              regexCriteria={/^[ A-Za-z0-9_@./#&+-]*$/}
+            />
+
+            <div className="flex w-full items-center justify-between text-sm font-normal">
+              <div className="flex gap-2">
+                <p>Resend in 0:59</p>
+              </div>
+
+              <p>
+                Forgot your Password?{" "}
+                <Link href="" className="font-bold">
+                  Resend OTP
+                </Link>
+              </p>
+            </div>
+
+            <button
+              type="submit"
+              className="h-[48px] w-full items-center justify-center rounded-[17px] bg-[#297FB8] text-base font-semibold"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      )}
     </main>
   );
 };
