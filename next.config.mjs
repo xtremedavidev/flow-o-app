0bdfeca85160a8bedb14a38aa7f42c2d8d9b0a56
Umoh Samuel Godwin <umohsg@gmail.com>
2024-10-23 23:52:50 +0100

    aliases

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+};
 
 export default nextConfig;
