8a3f2fc13e61f78cad0d2aca2591fa43d9fe7f23
Umoh Samuel Godwin <umohsg@gmail.com>
2024-10-24 00:08:52 +0100

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname, "src");
-    return config;
-  },
+  output: "standalone",
 };
 
 export default nextConfig;
